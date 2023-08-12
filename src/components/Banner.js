d93a93f0a6b657c293a6876d64e20ecb28a7dd45
Jayanth Krishnamurthy <regimeautomaton@gmail.com>
2023-08-12 16:51:42 -0500

    Finished final draft of assignment

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import data from './styling.css' 
+
+export default function Banner() {
+    return (
+        <div>
+            <h1>Orbit Report</h1>
+            <p>Click on buttons to see satellites in that orbit type</p>
+        </div>
+    );
+}
+
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,23 @@
-const Buttons = () => {
+import satData from "./satData";
+import data from './styling.css'
+
+const Buttons = ({filterByType, setSat, displaySats}) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+    <div className="flex-container">
+      {displaySats.map((sat, id) => { 
+        return ( 
+          <button onClick={() => filterByType(sat)} key={id}> 
+            {sat} Orbit 
+          </button> 
+        ); 
+      })} 
+
+
+      <button onClick = {() => setSat(satData)}>
+        All Orbits
+      </button>
+      <p></p>
+    </div>
   );
 };
 
